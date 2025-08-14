3378a1c8c634b262380f12d512d3fea041b77ebb
Naveen gaur <naveengaur343@gmail.com>
2025-08-14 16:17:52 +0530

    Create a Home Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <App />
-    <ToastContainer />
-  </>
+  <React.StrictMode>
+    
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+  </React.StrictMode>
 );
