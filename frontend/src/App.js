4a9311d274773a4e081f365f10cf385edd11b11f
Naveen gaur <naveengaur343@gmail.com>
2025-08-27 16:54:34 +0530

    i create a login or signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,39 @@
+// import "./App.css";
+// import { Route, Routes } from "react-router-dom";
+// import Home from './pages/Home';
+// import Navbar from './components/common/Navbar';
+
+// function App() {
+//   return(
+//     <div  className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+//       <Navbar/>
+//       <Routes>
+//           <Route path="/" element={<Home/>}></Route>
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Navbar from './components/common/Navbar';
 
+// 👇 in dono ko import karna hoga
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
-  return(
-    <div  className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
-      <Navbar/>
+  return (
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
       <Routes>
-          <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   )
@@ -16,4 +41,5 @@ function App() {
 
 export default App;
 
-// DUMMY COMMIT  
+
+
